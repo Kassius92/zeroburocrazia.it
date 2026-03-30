3ce0bed731e8c778830405f59a01c89ae5f52eb8
ZeroBurocrazia <info.zeroburocrazia@gmail.com>
2026-03-30 21:11:02 +0000

    New logo: zero with checkmark icon

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
   metadataBase: new URL('https://zeroburocrazia.it'),
   icons: {
     icon: [{ url: '/favicon.ico' }, { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }, { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' }],
-    apple: [{ url: '/appletouchicon.png' }],
+    apple: [{ url: '/apple-touch-icon.png' }],
   },
 };
 
